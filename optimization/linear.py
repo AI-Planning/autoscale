553642921fabb3526b83d5673ae11d19466afdec
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-09-13 14:18:46 +0200

    Let SMAC optimizer run planners.

--- optimization/linear.py
+++ optimization/linear.py
@@ -7,18 +7,22 @@ python3 linear.py
 """
 
 import argparse
+import datetime
 import logging
+import os
+import os.path
+import re
+import shlex
+import shutil
+import subprocess
+import sys
 
 import numpy as np
 
-# Import ConfigSpace and different types of parameters.
-from smac.configspace import ConfigurationSpace
-from ConfigSpace.hyperparameters import CategoricalHyperparameter, \
-    UniformFloatHyperparameter, UniformIntegerHyperparameter
-from ConfigSpace.conditions import InCondition
+from ConfigSpace.hyperparameters import UniformFloatHyperparameter
+from ConfigSpace.hyperparameters import UniformIntegerHyperparameter
 
-# Import SMAC utilities.
-from smac.tae.execute_func import ExecuteTAFuncDict
+from smac.configspace import ConfigurationSpace
 from smac.scenario.scenario import Scenario
 from smac.facade.smac_hpo_facade import SMAC4HPO
 
@@ -26,47 +30,125 @@ from smac.facade.smac_hpo_facade import SMAC4HPO
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--num-tasks", type=int, default=7,
-        help="Number of tasks to generate in each round (default: %(default)s)")
+        "--tasks", type=int, default=7,
+        help="Number of tasks to generate in each round (default: %(default)s, max=7)")
+    parser.add_argument(
+        "--evaluations", type=int, default=sys.maxsize,
+        help="Maximum number of configurations to evaluate (default: %(default)s)")
     parser.add_argument(
         "--optimization-time-limit", type=float, default=20 * 60 * 60,
         help="Maximum total time running planners (default: %(default)ss)")
+    parser.add_argument(
+        "smac_output_dir", help="Directory where to store logs and temporary files")
+    parser.add_argument(
+        "domain_file", help="Path to PDDL domain file")
+    parser.add_argument(
+        "generator_call", help="example: /path/to/gripper -n {y}")
+    parser.add_argument(
+        "images_dir", help="path to directory containing the Singularity images to run")
     return parser.parse_args()
 
 
+DIR = os.path.abspath(os.path.dirname(__file__))
+PLANNER_TIME_LIMIT = 180
+PLANNER_MEMORY_LIMIT = 3584  # 3.5 GiB
+MIN_PLANNER_RUNTIME = 0.1
 ARGS = parse_args()
+SMAC_OUTPUT_DIR = ARGS.smac_output_dir
+TMP_PLAN_DIR = "plan"
+SINGULARITY_SCRIPT = os.path.join(DIR, "run-singularity.sh")
+print("Singularity script:", SINGULARITY_SCRIPT)
+
+if ARGS.tasks > 7:
+    sys.exit("Error: number of tasks must be <= 7")
+
+if os.path.exists(SMAC_OUTPUT_DIR):
+    sys.exit("Error: SMAC output directory already exists")
+
+def run_planners(y):
+    # Exceptions are silently swallowed, so we catch them ourselves.
+    try:
+        # Write problem file.
+        plan_dir = os.path.join(SMAC_OUTPUT_DIR, TMP_PLAN_DIR)
+        shutil.rmtree(plan_dir, ignore_errors=True)
+        os.mkdir(plan_dir)
+        problem_file = os.path.join(plan_dir, "problem.pddl")
+        command = shlex.split(ARGS.generator_call.format(y=y))
+        logging.debug(f"Generator command: {command}")
+        with open(problem_file, "w") as f:
+            subprocess.run(command, stdout=f)
+
+        # Call planners.
+        runtimes = []
+        for image in sorted(os.listdir(ARGS.images_dir)):
+            if image.endswith((".img", ".simg")):
+                image_path = os.path.join(ARGS.images_dir, image)
+                logging.debug(f"Run image {image}")
+                planner_dir = os.path.join(plan_dir, image)
+                os.mkdir(planner_dir)
+
+                # Copy domain and problem into temporary dir.
+                domain_file = os.path.join(planner_dir, "domain.pddl")
+                shutil.copy2(ARGS.domain_file, domain_file)
+                shutil.copy2(problem_file, os.path.join(planner_dir, "problem.pddl"))
+                try:
+                    p = subprocess.run(
+                        [SINGULARITY_SCRIPT, image_path, "domain.pddl", "problem.pddl", "sas_plan"],
+                        cwd=planner_dir,
+                        stdout=subprocess.PIPE,
+                        timeout=PLANNER_TIME_LIMIT)
+                except subprocess.TimeoutExpired:
+                    logging.debug("Timeout occured")
+                else:
+                    # This only has a granularity of 1s, but should be enough.
+                    match = re.search(b"Singularity runtime: (.+)s", p.stdout)
+                    if match:
+                        runtime = float(match.group(1))
+                        runtime = max(MIN_PLANNER_RUNTIME, runtime)  # log(0) is undefined.
+                        runtimes.append(runtime)
+        print(f"Runtimes for y={y}: {runtimes}")
+        if runtimes:
+            return min(runtimes)
+        else:
+            return None
+    except Exception as err:
+        print(err)
+        raise
 
 
 def evaluate_cfg(cfg):
-    """
-    Parameters:
-    -----------
-    cfg: Configuration (ConfigSpace.ConfigurationSpace.Configuration)
-        Configuration containing the parameters.
-        Configurations are indexable!
+    n = ARGS.tasks
 
-    Returns:
-    --------
-    A crossvalidated mean score for the svm on the loaded data-set.
-    """
     print(cfg)
     m = cfg.get("m")
     b = cfg.get("b")
 
-    n = ARGS.num_tasks
-    t_star = [2**x for x in range(1, n + 1)]
-    t = [max(1, 2**x + m + 10 + m * b) for x in range(1, n + 1)]
+    Y = [int(m * x + b) for x in range(1, n + 1)]
 
-    print(t_star, t)
+    print("Y:", Y)
 
-    t_star = np.log2(np.array(t_star))
-    t = np.log2(np.array(t))
+    # TODO: make this a command line parameter.
+    if not all (1 <= y <= 1000 for y in Y):
+        print("y must be >= 1, skipping configuration")
+        return None
 
-    print(t_star, t)
+    opt_times = [2**x for x in range(1, n + 1)]
 
-    error = (((t_star - t) / t_star)**2).sum(axis=None)
-    print(t_star, t, error)
+    # TODO: pass individual timeout for each y.
+    min_times = []
+    for y in Y:
+        min_time = run_planners(y)
+        if min_time is None:
+            print(f"No planner solved task y={y}")
+            return None
+        else:
+            min_times.append(min_time)
+    #min_times = [max(1, 2**x + m + 10 + m * b) for x in range(1, n + 1)]  # for testing
 
+    opt_times = np.log2(np.array(opt_times))
+    min_times = np.log2(np.array(min_times))
+
+    error = (((opt_times - min_times) / opt_times)**2).sum(axis=None)
     return float(error)  # Minimize!
 
 logging.basicConfig(level=logging.INFO)  # logging.DEBUG for debug output
@@ -74,28 +156,42 @@ logging.basicConfig(level=logging.INFO)  # logging.DEBUG for debug output
 # Build Configuration Space which defines all parameters and their ranges.
 cs = ConfigurationSpace()
 
-m = UniformIntegerHyperparameter("m", lower=-100, upper=100, default_value=1, log=False)
-b = UniformIntegerHyperparameter("b", lower=-100, upper=100, default_value=0, log=False)
-cs.add_hyperparameters([m, b])
+# Default values are ignored for initial design
+# (see https://github.com/automl/SMAC3/issues/533). SMAC uses
+# (lower+upper)/2 instead. Since the initial design must not crash, we
+# need to select the bounds so that the (lower+upper)/2 is at least 1.
+# TODO: Allow negative values and enlarge domains.
+cs.add_hyperparameters([
+    UniformIntegerHyperparameter("b", lower=0, upper=10, default_value=0, log=False),
+    UniformFloatHyperparameter("m", lower=0.0, upper=2.0, default_value=1.0, log=False),
+])
 
 scenario = Scenario({
     "run_obj": "quality",
     # max. number of function evaluations
-    "runcount-limit": "inf",
-    "algo_runs_timelimit": ARGS.optimization_time_limit,
+    "ta_run_limit": ARGS.evaluations,
+    "wallclock_limit": ARGS.optimization_time_limit,
     "cs": cs,
     "deterministic": "true",
-    # memory limit for target algorithm (3.5 GB)
-    "memory-limit": 3584,
+    # memory limit for evaluate_cfg
+    "memory_limit": PLANNER_MEMORY_LIMIT,
+    # time limit for evaluate_cfg (we cut off planner runs ourselves)
+    "cutoff": None,
+    "output_dir": SMAC_OUTPUT_DIR,
 })
 
 # Example call of the function
-#def_value = evaluate_cfg(cs.get_default_configuration())
-#print("Default Value: %.2f" % (def_value))
+default_cfg = cs.get_default_configuration()
+print("Default config:", default_cfg)
+#evaluate_cfg(default_cfg)
 
 print("Optimizing...")
 smac = SMAC4HPO(
-    scenario=scenario, rng=np.random.RandomState(42), tae_runner=evaluate_cfg)
+    scenario=scenario,
+    rng=np.random.RandomState(42),
+    tae_runner=evaluate_cfg)
+print("Output dir:", SMAC_OUTPUT_DIR)
+print("SMAC output dir:", smac.output_dir)
 incumbent = smac.optimize()
 inc_value = evaluate_cfg(incumbent)
 print("Optimized value: %.2f" % (inc_value))
