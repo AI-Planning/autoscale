8245c384c74018546efcb33e069bc62d6f2994fa
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-11-05 14:57:22 +0100

    Add experiment.

---
+++ optimization/2019-11-04-A-opt-10-domains.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python
+
+import os
+import platform
+
+from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+from lab.experiment import Experiment
+
+from downward.reports.absolute import AbsoluteReport
+
+
+# Create custom report class with suitable info and error attributes.
+class BaseReport(AbsoluteReport):
+    INFO_ATTRIBUTES = ['run_time_limit', 'run_memory_limit']
+    ERROR_ATTRIBUTES = [
+        'domain', 'problem', 'algorithm', 'unexplained_errors', 'error', 'node']
+
+
+NODE = platform.node()
+REMOTE = NODE.endswith(".scicore.unibas.ch") or NODE.endswith(".cluster.bc2.ch")
+DIR = os.path.abspath(os.path.dirname(__file__))
+REPO = os.path.dirname(DIR)
+IMAGES_DIR = os.path.join(REPO, "images")
+DOMAINS = [
+    "blocksworld",
+    "driverlog",
+    "gripper",
+    "miconic-strips",
+    "rover",
+    "satellite",
+    "tpp",
+    "trucks",
+    "visitall",
+    "woodworking",
+    "zenotravel",
+]
+ATTRIBUTES = [
+    "error", "all_*", "final_configuration*",
+    "configurations", "optimization_wallclock_time",
+]
+RUN_TIME_LIMIT = 23 * 60 * 60
+RUN_MEMORY_LIMIT = 3584
+
+if REMOTE:
+    ENV = BaselSlurmEnvironment(
+        email="jendrik.seipp@unibas.ch",
+        setup="\n".join([
+            BaselSlurmEnvironment.DEFAULT_SETUP,
+            "module load Singularity/2.6.1 2> /dev/null",
+            "source activate smac-conda",
+        ]))
+    SMAC_TIME_LIMIT = 10 * 60 * 60
+    OPTIONS = []
+else:
+    ENV = LocalEnvironment(processes=1)
+    SMAC_TIME_LIMIT = 10
+    OPTIONS = ["--debug", "--evaluations", "1", "--tasks", "2", "--tasksbaseline", "2"]
+    DOMAINS = ["miconic-strips"]
+
+
+exp = Experiment(environment=ENV)
+exp.add_parser('exp-parser.py')
+
+for domain in DOMAINS:
+    run = exp.add_run()
+    run.add_command(
+        'optimize',
+        ['python3', os.path.join(DIR, 'linear.py'), "--opt", str(SMAC_TIME_LIMIT), IMAGES_DIR, domain] + OPTIONS,
+        time_limit=RUN_TIME_LIMIT,
+        memory_limit=RUN_MEMORY_LIMIT,
+        hard_stdout_limit=50 * 1024)
+    problem = "optimize"
+    algorithm = "linear"
+    run.set_property('run_time_limit', RUN_TIME_LIMIT)
+    run.set_property('run_memory_limit', RUN_MEMORY_LIMIT)
+    run.set_property('domain', domain)
+    run.set_property('problem', problem)
+    run.set_property('algorithm', algorithm)
+    # Every run has to have a unique id in the form of a list.
+    run.set_property('id', [domain, problem, algorithm])
+
+exp.add_step('build', exp.build)
+exp.add_step('start', exp.start_runs)
+exp.add_fetcher(name='fetch')
+
+exp.add_report(
+    BaseReport(attributes=ATTRIBUTES),
+    outfile='{}.html'.format(exp.name))
+
+# Parse the commandline and run the specified steps.
+exp.run_steps()
