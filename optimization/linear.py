5489f8d27746d5000de8c3b0a1eb933d51356091
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-11-06 23:20:18 +0100

    Add experiment for 16 domains.

--- optimization/linear.py
+++ optimization/linear.py
@@ -468,7 +468,7 @@ DOMAIN_LIST = [
 
 DOMAIN_DICT = {d.name: d for d in DOMAIN_LIST}
 
-print(DOMAIN_DICT.keys())
+print("Available domains: {}".format(sorted(DOMAIN_DICT)))
 
 for domain in DOMAIN_DICT:
     assert os.path.exists(os.path.join(ARGS.generators_dir, domain, "domain.pddl")), f"domain.pddl missing for {domain}"
