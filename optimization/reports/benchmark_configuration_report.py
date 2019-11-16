1a6852c4e418e1f3e72cc7a9b87aed65108b19be
Jendrik Seipp <jendrik.seipp@unibas.ch>
2019-11-16 23:58:14 +0100

    Add OPT results to benchmark generation script.

--- optimization/reports/benchmark_configuration_report.py
+++ optimization/reports/benchmark_configuration_report.py
@@ -1,6 +1,7 @@
 import ast
 from collections import defaultdict
 import json
+import pprint
 import sys
 
 from downward.reports import PlanningReport
@@ -13,13 +14,16 @@ class BenchmarkConfigurationReport(PlanningReport):
     def write(self):
         best_configs = {}
         for (domain, algo), runs in sorted(self.domain_algorithm_runs.items()):
+            domain = domain.replace("-baseline-and-sart", "")
             best_run = min(runs, key=lambda run: run.get("final_value", sys.maxsize))
             best_value = best_run.get("final_value")
             if best_value is None:
                 print(f"\nERROR: all runs for {domain} failed\n", file=sys.stderr)
             else:
                 best_configuration = ast.literal_eval(best_run["final_configuration"])
-                best_configs[domain] = best_configuration
+                baseline_runtimes = ast.literal_eval(best_run["final_baseline_runtimes"])
+                sart_runtimes = ast.literal_eval(best_run["final_sart_runtimes"])
+                best_configs[domain] = (best_configuration, baseline_runtimes, sart_runtimes)
                 print(f"{domain}:")
                 for attr in [
                         "final_value", "final_configuration",
@@ -29,5 +33,6 @@ class BenchmarkConfigurationReport(PlanningReport):
                     print(f"  {attr}: {best_run[attr]}")
                 print()
 
-        print(json.dumps(best_configs, sort_keys=True, indent=4))
+        #print(json.dumps(best_configs, sort_keys=True, indent=1))
+        pprint.pprint(best_configs, indent=1)
 
