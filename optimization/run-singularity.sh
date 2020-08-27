416487accd8a6b47ee8398866de74b9d1ae69265
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-08-28 00:28:13 +0200

    Make redirects a little more intuitive.

--- optimization/run-singularity.sh
+++ optimization/run-singularity.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 if [[ $# != 4 ]]; then
-    echo "usage: $(basename "$0") image domain_file problem_file plan_file" 1>&2
+    >&2 echo "usage: $(basename "$0") image domain_file problem_file plan_file"
     exit 2
 fi
 
 if [ -f $PWD/$4 ]; then
-    echo "Remove $PWD/$4" 1>&2
+    >&2 echo "Error: remove $PWD/$4"
     exit 2
 fi
 
@@ -16,9 +16,7 @@ set +e
 /usr/bin/time -o /dev/stdout -f "Singularity runtime: %es real, %Us user, %Ss sys" singularity run -C -H $PWD $1 $PWD/$2 $PWD/$3 $4
 set -e
 
-echo ""
-echo "Run VAL"
-echo ""
+printf "\nRun VAL\n\n"
 
 if [ -f $PWD/$4 ]; then
     echo "Found plan file."
