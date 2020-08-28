b43126ed96330adea66f9225aa3f86846454c07d
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-08-29 00:40:20 +0200

    Fix syntax.

--- optimization/run-singularity.sh
+++ optimization/run-singularity.sh
@@ -14,7 +14,7 @@ fi
 
 set +e
 # Some planners print to stderr when running out of memory, so we redirect stderr to stdout.
-{ /usr/bin/time -o /dev/stdout -f "Singularity runtime: %es real, %Us user, %Ss sys" singularity run -C -H $PWD $1 $PWD/$2 $PWD/$3 $4 } 2>&1
+{ /usr/bin/time -o /dev/stdout -f "Singularity runtime: %es real, %Us user, %Ss sys" singularity run -C -H $PWD $1 $PWD/$2 $PWD/$3 $4 ; } 2>&1
 set -e
 
 printf "\nRun VAL\n\n"
