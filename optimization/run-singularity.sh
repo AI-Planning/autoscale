24f5c302fbb40f42c31f8095949b859a5d6a31fc
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-08-28 23:37:30 +0200

    Redirect planner stderr to stdout again.

--- optimization/run-singularity.sh
+++ optimization/run-singularity.sh
@@ -3,17 +3,18 @@
 set -euo pipefail
 
 if [[ $# != 4 ]]; then
-    >&2 echo "usage: $(basename "$0") image domain_file problem_file plan_file"
+    echo "usage: $(basename "$0") image domain_file problem_file plan_file" 1>&2
     exit 2
 fi
 
 if [ -f $PWD/$4 ]; then
-    >&2 echo "Error: remove $PWD/$4"
+    echo "Error: remove $PWD/$4" 1>&2
     exit 2
 fi
 
 set +e
-/usr/bin/time -o /dev/stdout -f "Singularity runtime: %es real, %Us user, %Ss sys" singularity run -C -H $PWD $1 $PWD/$2 $PWD/$3 $4
+# Some planners print to stderr when running out of memory, so we redirect stderr to stdout.
+{ /usr/bin/time -o /dev/stdout -f "Singularity runtime: %es real, %Us user, %Ss sys" singularity run -C -H $PWD $1 $PWD/$2 $PWD/$3 $4 } 2>&1
 set -e
 
 printf "\nRun VAL\n\n"
