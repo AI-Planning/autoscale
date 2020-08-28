3e53339b2532e6f49ce068f83a29077a2125ae76
Silvan Sievers <silvan.sievers@unibas.ch>
2020-08-28 17:50:26 +0200

    large renaming of planners

--- scripts/build-all-images.sh
+++ scripts/build-all-images.sh
@@ -6,39 +6,40 @@ cd $(dirname "$0")
 
 OUTDIR=../images/
 
-./build-image.sh ../planners/custom/seq-opt/fd-19.06/Singularity-blind ${OUTDIR}/blind.img
-./build-image.sh ../planners/custom/seq-opt/fd-19.06/Singularity-fdss1-01 ${OUTDIR}/fdss-mas1.img
-./build-image.sh ../planners/custom/seq-opt/fd-19.06/Singularity-fdss1-02 ${OUTDIR}/fdss-mas2.img
-./build-image.sh ../planners/custom/seq-opt/fd-19.06/Singularity-fdss1-03 ${OUTDIR}/bjolp.img
-./build-image.sh ../planners/custom/seq-opt/fd-19.06/Singularity-fdss1-04 ${OUTDIR}/lmcut.img
-./build-image.sh ../planners/ipc-2014/seq-opt/symba/Singularity-01 ${OUTDIR}/symba1.img
-./build-image.sh ../planners/ipc-2014/seq-opt/symba/Singularity-02 ${OUTDIR}/symba2.img
-./build-image.sh ../planners/ipc-2018/seq-opt/complementary2/Singularity-180s ${OUTDIR}/complementary2-3584mb-180s.img
-./build-image.sh ../planners/ipc-2018/seq-opt/complementary2/Singularity-1800s ${OUTDIR}/complementary2-3584mb-1800s.img
-./build-image.sh ../planners/ipc-2018/seq-opt/delfi1/Singularity-blind ${OUTDIR}/delfi-blind.img
-./build-image.sh ../planners/ipc-2018/seq-opt/delfi1/Singularity-celmcut ${OUTDIR}/delfi-celmcut.img
-./build-image.sh ../planners/ipc-2018/seq-opt/delfi1/Singularity-ipdb-60s ${OUTDIR}/delfi-ipdb-60s.img
-./build-image.sh ../planners/ipc-2018/seq-opt/delfi1/Singularity-ipdb-1800s ${OUTDIR}/delfi-ipdb-1800s.img
-./build-image.sh ../planners/ipc-2018/seq-opt/delfi1/Singularity-mas-sccdfp-60s ${OUTDIR}/delfi-mas-sccdfp-60s.img
-./build-image.sh ../planners/ipc-2018/seq-opt/delfi1/Singularity-mas-sccdfp-1800s ${OUTDIR}/delfi-mas-sccdfp-1800s.img
-./build-image.sh ../planners/ipc-2018/seq-opt/delfi1/Singularity-mas-miasm ${OUTDIR}/delfi-mas-miasm.img
-./build-image.sh ../planners/ipc-2018/seq-opt/decstar/Singularity-fork ${OUTDIR}/decstar-opt-fork.img || true
-./build-image.sh ../planners/ipc-2018/seq-opt/decstar/Singularity-star ${OUTDIR}/decstar-opt-star.img || true
-./build-image.sh ../planners/ipc-2018/seq-opt/decstar/Singularity ${OUTDIR}/decstar-opt.img || true
-./build-image.sh ../planners/ipc-2018/seq-opt/scorpion/Singularity ${OUTDIR}/scorpion.img
-./build-image.sh ../planners/custom/seq-opt/scorpion-nodiv/Singularity ${OUTDIR}/scorpion-nodiv.img
+./build-image.sh ../planners/fd-19.06/Singularity-blind ${OUTDIR}/fd1906-blind.img
+./build-image.sh ../planners/fd-19.06/Singularity-fdss1-mas1 ${OUTDIR}/fd1906-fdss1-mas1.img
+./build-image.sh ../planners/fd-19.06/Singularity-fdss1-mas2 ${OUTDIR}/fd1906-fdss1-mas2.img
+./build-image.sh ../planners/fd-19.06/Singularity-bjolp ${OUTDIR}/fd1906-bjolp.img
+./build-image.sh ../planners/fd-19.06/Singularity-lmcut ${OUTDIR}/fd1906-lmcut.img
+./build-image.sh ../planners/ipc2014-opt-symba/Singularity-01 ${OUTDIR}/ipc2014-opt-symba1.img
+## symba2 seems to not be used
+./build-image.sh ../planners/ipc2014-opt-symba/Singularity-02 ${OUTDIR}/ipc2014-opt-symba2.img
+./build-image.sh ../planners/ipc2018-opt-complementary2-3584mb/Singularity-180s ${OUTDIR}/ipc2018-opt-complementary2-3584mb-180s.img
+./build-image.sh ../planners/ipc2018-opt-complementary2-3584mb/Singularity-1800s ${OUTDIR}/ipc2018-opt-complementary2-3584mb-1800s.img
+./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-blind ${OUTDIR}/ipc2018-opt-delfi-blind.img
+./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-celmcut ${OUTDIR}/ipc2018-opt-delfi-celmcut.img
+./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-ipdb-60s ${OUTDIR}/ipc2018-opt-delfi-ipdb-60s.img
+./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-ipdb-1800s ${OUTDIR}/ipc2018-opt-delfi-ipdb-1800s.img
+./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-mas-sccdfp-60s ${OUTDIR}/ipc2018-opt-delfi-mas-sccdfp-60s.img
+./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-mas-sccdfp-1800s ${OUTDIR}/ipc2018-opt-delfi-mas-sccdfp-1800s.img
+./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-mas-miasm ${OUTDIR}/ipc2018-opt-delfi-mas-miasm.img
+./build-image.sh ../planners/ipc2018-opt-decstar/Singularity-fork ${OUTDIR}/ipc2018-opt-decstar-fork.img || true
+./build-image.sh ../planners/ipc2018-opt-decstar/Singularity-star ${OUTDIR}/ipc2018-opt-decstar-star.img || true
+./build-image.sh ../planners/ipc2018-opt-decstar/Singularity ${OUTDIR}/ipc2018-opt-decstar.img || true
+./build-image.sh ../planners/ipc2018-opt-scorpion/Singularity ${OUTDIR}/ipc2018-opt-scorpion.img
+./build-image.sh ../planners/ipc2018-opt-scorpion-nodiv/Singularity ${OUTDIR}/ipc2018-opt-scorpion-nodiv.img
 
-./build-image.sh ../planners/custom/seq-opt/fd-19.06/Singularity-gbfs-ff ${OUTDIR}/gbfs-ff.img
-./build-image.sh ../planners/custom/seq-opt/fd-19.06/Singularity-lama-first ${OUTDIR}/lama-first.img
-./build-image.sh ../planners/sat/decstar/Singularity ${OUTDIR}/decstar-agl.img
-./build-image.sh ../planners/sat/fast-downward-remix-agl/Singularity ${OUTDIR}/remix-agl.img
-./build-image.sh ../planners/sat/lapkt-dual-bfws/Singularity ${OUTDIR}/lapkt-dual-bfws.img
-./build-image.sh ../planners/sat/lapkt-bfws-pref/Singularity ${OUTDIR}/lapkt-bfws-pref.img
-./build-image.sh ../planners/sat/lapkt-poly-bfws/Singularity ${OUTDIR}/lapkt-poly-bfws.img
-./build-image.sh ../planners/sat/olcff/Singularity ${OUTDIR}/olcff.img
-./build-image.sh ../planners/sat/madagascar/Singularity-MpC ${OUTDIR}/mpc.img
-./build-image.sh ../planners/sat/saarplan/Singularity-dec-fallback ${OUTDIR}/saarplan-dec-fallback.img
-./build-image.sh ../planners/sat/saarplan/Singularity-grey ${OUTDIR}/saarplan-grey.img
-./build-image.sh ../planners/sat/saarplan/Singularity ${OUTDIR}/saarplan-agl.img
+./build-image.sh ../planners/fd-19.06/Singularity-gbfs-ff ${OUTDIR}/fd1906-gbfs-ff.img
+./build-image.sh ../planners/fd-19.06/Singularity-lama-first ${OUTDIR}/fd1906-lama-first.img
+./build-image.sh ../planners/ipc2018-agl-decstar/Singularity ${OUTDIR}/ipc2018-agl-decstar-agl.img
+./build-image.sh ../planners/ipc2018-agl-fd-remix/Singularity ${OUTDIR}/ipc2018-agl-fd-remix.img
+./build-image.sh ../planners/ipc2018-agl-lapkt-dual-bfws/Singularity ${OUTDIR}/ipc2018-agl-lapkt-dual-bfws.img
+./build-image.sh ../planners/ipc2018-agl-lapkt-bfws-pref/Singularity ${OUTDIR}/ipc2018-agl-lapkt-bfws-pref.img
+./build-image.sh ../planners/ipc2018-agl-lapkt-poly-bfws/Singularity ${OUTDIR}/ipc2018-agl-lapkt-poly-bfws.img
+./build-image.sh ../planners/ipc2018-agl-olcff/Singularity ${OUTDIR}/ipc2018-agl-olcff.img
+./build-image.sh ../planners/ipc2014-agl-madagascar/Singularity-MpC ${OUTDIR}/ipc2014-agl-mpc.img
+./build-image.sh ../planners/ipc2018-agl-saarplan/Singularity-dec-fallback ${OUTDIR}/ipc2018-agl-saarplan-dec-fallback.img
+./build-image.sh ../planners/ipc2018-agl-saarplan/Singularity-grey ${OUTDIR}/ipc2018-agl-saarplan-grey.img
+./build-image.sh ../planners/ipc2018-agl-saarplan/Singularity ${OUTDIR}/ipc2018-agl-saarplan-agl.img
 
 echo "Finished building images"
