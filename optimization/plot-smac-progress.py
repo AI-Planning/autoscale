fb0c17d581e04f18afc51e9a76366b4442b82c34
Jendrik Seipp <jendrik.seipp@unibas.ch>
2020-09-01 10:27:38 +0200

    Add script to plot SMAC progress.

---
+++ optimization/plot-smac-progress.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import typer
+
+
+def main(expdir : Path):
+    result_dir = expdir / "smac-plots"
+    result_dir.mkdir(exist_ok=True)
+    for json_file in expdir.glob("smac-output-*/run_*/traj.json"):
+        df = pd.read_json(json_file, lines=True)
+        print(df)
+        df.plot(x="cpu_time", y="cost")
+        #plt.show()
+        domain_dir = result_dir / json_file.parts[-3]
+        domain_dir.mkdir(exist_ok=True)
+        plt.savefig(domain_dir / json_file.parts[-2])
+
+
+if __name__ == "__main__":
+    typer.run(main)
