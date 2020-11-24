f7939da03f0e94f9e0918a25707526d712cafcd7
Silvan Sievers <silvan.sievers@unibas.ch>
2020-11-24 16:53:04 +0100

    change planner selection

--- scripts/build-all-images.sh
+++ scripts/build-all-images.sh
@@ -21,8 +21,7 @@ OUTDIR=../images/
 ./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-ipdb ${OUTDIR}/ipc2018-opt-delfi-ipdb.img
 ./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-mas-sccdfp ${OUTDIR}/ipc2018-opt-delfi-mas-sccdfp.img
 ./build-image.sh ../planners/ipc2018-opt-delfi1/Singularity-mas-miasm ${OUTDIR}/ipc2018-opt-delfi-mas-miasm.img
-./build-image.sh ../planners/ipc2018-opt-decstar/Singularity-fork ${OUTDIR}/ipc2018-opt-decstar-fork.img || true # this does *not* run any fallback search
-./build-image.sh ../planners/ipc2018-opt-decstar/Singularity-star ${OUTDIR}/ipc2018-opt-decstar-star.img || true # this does *not* run any fallback search
+./build-image.sh ../planners/ipc2018-opt-decstar/Singularity ${OUTDIR}/ipc2018-opt-decstar.img
 ./build-image.sh ../planners/ipc2018-opt-scorpion/Singularity ${OUTDIR}/ipc2018-opt-scorpion.img
 
 # Optimization SAT/AGL
@@ -34,14 +33,13 @@ OUTDIR=../images/
 ./build-image.sh ../planners/ipc2014-agl-probe/Singularity ${OUTDIR}/ipc2014-agl-probe.img
 
 # Evaluation SAT/AGL
-./build-image.sh ../planners/ipc2018-agl-decstar/Singularity-fork ${OUTDIR}/ipc2018-agl-decstar-fork.img || true # this does *not* run any fallback search
+./build-image.sh ../planners/ipc2018-agl-decstar/Singularity ${OUTDIR}/ipc2018-agl-decstar.img
 ./build-image.sh ../planners/ipc2018-agl-fd-remix/Singularity ${OUTDIR}/ipc2018-agl-fd-remix.img
 ./build-image.sh ../planners/ipc2018-agl-lapkt-dual-bfws/Singularity ${OUTDIR}/ipc2018-agl-lapkt-dual-bfws.img
 ./build-image.sh ../planners/ipc2018-agl-lapkt-bfws-pref/Singularity ${OUTDIR}/ipc2018-agl-lapkt-bfws-pref.img
 ./build-image.sh ../planners/ipc2018-agl-lapkt-poly-bfws/Singularity ${OUTDIR}/ipc2018-agl-lapkt-poly-bfws.img
 ./build-image.sh ../planners/ipc2018-agl-olcff/Singularity ${OUTDIR}/ipc2018-agl-olcff.img
-./build-image.sh ../planners/ipc2018-agl-saarplan/Singularity-grey ${OUTDIR}/ipc2018-agl-saarplan-grey.img
-./build-image.sh ../planners/ipc2018-agl-saarplan/Singularity-fork ${OUTDIR}/ipc2018-agl-saarplan-fork.img || true # this does *not* run any fallback search
+./build-image.sh ../planners/ipc2018-agl-saarplan/Singularity ${OUTDIR}/ipc2018-agl-saarplan.img
 ./build-image.sh ../planners/ipc2018-agl-cerberus/Singularity ${OUTDIR}/ipc2018-agl-cerberus.img
 
 echo "Finished building images"
