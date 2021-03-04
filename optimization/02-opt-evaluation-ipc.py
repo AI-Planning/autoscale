a686543af9e1badf53fdf43c3c5d90980aad76c7
Jendrik Seipp <jendrik.seipp@liu.se>
2021-03-04 22:11:29 +0100

    Remove old experiments and results.

--- optimization/02-opt-evaluation-ipc.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-import project
-
-exp = project.get_combination_experiment()
-project.fetch_algorithms(exp, "2020-09-03-E-evaluation-opt-ipc")
-project.add_evaluation_reports(exp)
-
-exp.run_steps()
--- optimization/05-sat-evaluation-ipc.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-import project
-
-exp = project.get_combination_experiment()
-project.fetch_algorithms(exp, "2020-09-03-F-evaluation-sat-ipc")
-project.add_evaluation_reports(exp)
-
-exp.run_steps()
