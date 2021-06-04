34f0243da78ec1d122e06d0c5635f097a0065586
Jendrik Seipp <jendrik.seipp@liu.se>
2021-06-04 13:35:11 +0200

    Fix types in Mystery domain.

--- autoscale/run-singularity.sh
+++ autoscale/run-singularity.sh
@@ -26,10 +26,10 @@ printf "\nRun VAL\n\n"
 
 if [ -f $PWD/$4 ]; then
     echo "Found plan file."
-    validate "$PWD/$2" "$PWD/$3" "$PWD/$4"
+    validate -v "$PWD/$2" "$PWD/$3" "$PWD/$4"
     exit 0
 else
     echo "No plan file."
-    validate "$PWD/$2" "$PWD/$3"
+    validate -v "$PWD/$2" "$PWD/$3"
     exit 99
 fi
