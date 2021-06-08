0d9c2b2fd756cb775a7d2b301ebd655a3dcf98e3
Jendrik Seipp <jendrik.seipp@liu.se>
2021-06-08 13:26:21 +0200

    Add exps.

---
+++ experiments/2021-06-08-A-smac-opt-termes-2018-20h.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import project
+
+
+DOMAINS = [
+    "termes",
+]
+NUMERIC_ATTRIBUTES = [
+    project.Attribute("final_value", min_wins=True),
+    project.Attribute("evaluated_configurations", min_wins=False),
+    project.Attribute("optimization_wallclock_time", min_wins=None),
+    project.Attribute("incumbent_changed", min_wins=False),
+    project.Attribute("evaluation_time", min_wins=False),
+]
+ATTRIBUTES = NUMERIC_ATTRIBUTES + [
+    "error", "run_dir", "final_*",
+]
+EXTRA_OPTIONS = []
+if project.REMOTE:
+    SMAC_RUNS_PER_DOMAIN = 10
+else:
+    SMAC_RUNS_PER_DOMAIN = 1
+    EXTRA_OPTIONS += ["--debug", "--evaluations", "1", "--tasks", "3", "--tasksbaseline", "3"]
+    DOMAINS = ["driverlog"]
+
+
+exp = project.get_smac_experiment(DOMAINS, SMAC_RUNS_PER_DOMAIN, ATTRIBUTES, NUMERIC_ATTRIBUTES, EXTRA_OPTIONS)
+exp.run_steps()
