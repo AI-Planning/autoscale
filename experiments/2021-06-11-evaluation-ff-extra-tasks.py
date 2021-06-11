4da4603c0eb5ff1f4fd10c94f8df0e5388f5f1fc
Silvan Sievers <silvan.sievers@unibas.ch>
2021-06-11 11:34:22 +0200

    add test evaluation exp

---
+++ experiments/2021-06-11-evaluation-ff-extra-tasks.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import project
+
+import pathlib
+
+BENCHMARKS_DIR = (pathlib.Path(project.REPO) / "tasks-of-domains-without-usable-generator").resolve()
+DOMAINS = [
+    "agricola", "airport", "caldera", "freecell", "ged", "mprime",
+    "mystery", "organic-synthesis-split", "parcprinter", "pathways",
+    "pegsol", "pipesworld-notankage", "pipesworld-tankage",
+    "psr-small", "sokoban", "tetris", "thoughtful", "tidybot"
+]
+PLANNER = [
+    "fd1906-gbfs-ff",
+]
+ATTRIBUTES = ["cost", "coverage", "error", "run_dir", "runtime", "unsolvable"]
+
+exp = project.get_evaluation_experiment(
+    planners=PLANNER,
+    benchmarks_dir="",
+    domains=DOMAINS,
+    attributes=ATTRIBUTES,
+    time_limit=60,
+    abs_benchmarks_dir=BENCHMARKS_DIR)
+
+exp.run_steps()
--- experiments/project.py
+++ experiments/project.py
@@ -375,7 +375,7 @@ def get_smac_experiment(
 
 
 def get_evaluation_experiment(
-    planners, benchmarks_dir, domains, attributes, environment=None, time_limit=1800):
+    planners, benchmarks_dir, domains, attributes, environment=None, time_limit=1800, abs_benchmarks_dir=None):
     """
     *bechmarks_dir* can either be an absolute path or a directory name under ^/benchmarks/.
     """
@@ -406,7 +406,8 @@ def get_evaluation_experiment(
     exp.add_resource("run_singularity", singularity_script)
 
     suite = []
-    abs_benchmarks_dir = (Path(REPO) / "benchmarks" / benchmarks_dir).resolve()
+    if not abs_benchmarks_dir:
+        abs_benchmarks_dir = (Path(REPO) / "benchmarks" / benchmarks_dir).resolve()
     for domain in domains:
         suite.extend(suites.build_suite(abs_benchmarks_dir, [domain]))
     if not REMOTE:
