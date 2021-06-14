1cc0eeaea5eff550af9b6da62aaf167c8e7433da
Silvan Sievers <silvan.sievers@unibas.ch>
2021-06-14 16:36:55 +0200

    evaluation exps

--- experiments/2021-06-14-A-evaluation-opt-extra-tasks.py
+++ experiments/2021-06-14-A-evaluation-opt-extra-tasks.py
@@ -26,7 +26,21 @@ DOMAINS = [
     "tidybot",
 ]
 PLANNER = [
-    "fd1906-gbfs-ff",
+    "fd1906-blind",
+    "fd1906-fdss1-mas1-60s",
+    "fd1906-fdss1-mas2-60s",
+    "fd1906-bjolp",
+    "fd1906-lmcut",
+    "ipc2014-opt-symba1",
+
+    "ipc2018-opt-complementary2-3584mb",
+    "ipc2018-opt-decstar",
+    "ipc2018-opt-delfi-blind",
+    "ipc2018-opt-delfi-celmcut",
+    "ipc2018-opt-delfi-ipdb",
+    "ipc2018-opt-delfi-mas-miasm",
+    "ipc2018-opt-delfi-mas-sccdfp",
+    "ipc2018-opt-scorpion",
 ]
 ATTRIBUTES = ["cost", "coverage", "error", "run_dir", "runtime", "unsolvable"]
 
