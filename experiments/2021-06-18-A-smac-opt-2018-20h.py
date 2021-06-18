d56e7f22c360b962aab084651db74c8d74f1ce44
Jendrik Seipp <jendrik.seipp@liu.se>
2021-06-18 16:19:51 +0200

    Add SMAC experiments.

---
+++ experiments/2021-06-18-A-smac-opt-2018-20h.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import project
+
+
+DOMAINS = [
+    "barman", "blocksworld", "childsnack", "data-network", "depots", "driverlog",
+    "elevators", "floortile", "grid", "gripper", "hiking", "logistics", "miconic",
+    "nomystery", "openstacks", "parking", "rovers", "satellite",
+    "scanalyzer", "snake", "storage", "termes", "tpp", "transport",
+    "visitall", "woodworking", "zenotravel",
+]
+NUMERIC_ATTRIBUTES = [
+    project.Attribute("final_value", min_wins=True),
+    project.Attribute("evaluated_configurations", min_wins=False),
+    project.Attribute("optimization_wallclock_time", min_wins=None),
+    project.Attribute("incumbent_changed", min_wins=False),
+    project.Attribute("evaluation_time", min_wins=False),
+]
+ATTRIBUTES = NUMERIC_ATTRIBUTES + [
+    "error", "run_dir", "final_*", "max_shared_runs",
+]
+EXTRA_OPTIONS = ["--database", project.DIR / "results" / "2021-06-02-A-smac-opt-agricola-termes-2018-20h.json"]
+if project.REMOTE:
+    SMAC_RUNS_PER_DOMAIN = 5
+else:
+    SMAC_RUNS_PER_DOMAIN = 2
+    EXTRA_OPTIONS += ["--debug", "--evaluations", "1", "--tasks", "3", "--tasksbaseline", "3"]
+    DOMAINS = ["driverlog"]
+
+
+exp = project.get_smac_experiment(DOMAINS, SMAC_RUNS_PER_DOMAIN, ATTRIBUTES, NUMERIC_ATTRIBUTES, EXTRA_OPTIONS)
+exp.run_steps()
