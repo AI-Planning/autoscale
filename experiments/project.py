a19c1e225a857b779792e5d5bfe346f31dbb6a6a
Jendrik Seipp <jendrik.seipp@liu.se>
2021-07-02 11:20:31 +0200

    Add SMAC experiment.

--- experiments/project.py
+++ experiments/project.py
@@ -320,7 +320,7 @@ def get_smac_experiment(
 
     environment = LocalEnvironment(processes=2)
     if REMOTE:
-        partition = "infai_2" #"infai_1" if track == "opt" else "infai_2"
+        partition = "infai_1" if track == "opt" else "infai_2"
         environment = BaselSlurmEnvironment(
             email=USER.email,
             partition=partition)
