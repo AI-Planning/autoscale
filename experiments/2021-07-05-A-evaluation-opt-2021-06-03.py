d2edcfd29ad1e4038b42c91198a77b007c3519a3
Silvan Sievers <silvan.sievers@unibas.ch>
2021-07-09 23:25:18 +0200

    update evaluation exps

--- experiments/2021-07-05-A-evaluation-opt-2021-06-03.py
+++ experiments/2021-07-05-A-evaluation-opt-2021-06-03.py
@@ -4,7 +4,9 @@ import project
 
 import pathlib
 
-BENCHMARKS_DIR = "2021-06-30-opt"
+from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+
+BENCHMARKS_DIR = pathlib.Path("2021-06-30-opt")
 DOMAINS = [
     "barman", "blocksworld", "childsnack", "data-network", "depots",
     "driverlog", "elevators", "floortile", "grid", "gripper", "hiking",
@@ -29,13 +31,35 @@ PLANNERS = [
     "ipc2018-opt-delfi-mas-sccdfp",
     "ipc2018-opt-scorpion",
 ]
-ATTRIBUTES = ["cost", "coverage", "error", "run_dir", "runtime", "unsolvable", "completely_explored", "virtual_memory"]
+ATTRIBUTES = [
+    "cost", "coverage", "error", "run_dir", "runtime", "unsolvable",
+    "completely_explored", "virtual_memory", "out_of_time",
+    "out_of_memory"
+]
+ENVIRONMENT = BaselSlurmEnvironment(
+    partition="infai_2",
+    memory_per_cpu="6354M",
+    cpus_per_task=1,
+    email=project.USER.email,
+    export=[],
+    # paths obtained via:
+    # module purge
+    # module -q load CMake/3.15.3-GCCcore-8.3.0
+    # module -q load GCC/8.3.0
+    # echo $PATH
+    # echo $LD_LIBRARY_PATH
+    setup='export PATH=/scicore/soft/apps/binutils/2.32-GCCcore-8.3.0/bin:/scicore/soft/apps/CMake/3.15.3-GCCcore-8.3.0/bin:/scicore/soft/apps/cURL/7.66.0-GCCcore-8.3.0/bin:/scicore/soft/apps/bzip2/1.0.8-GCCcore-8.3.0/bin:/scicore/soft/apps/ncurses/6.1-GCCcore-8.3.0/bin:/scicore/soft/apps/GCCcore/8.3.0/bin:/infai/sieverss/repos/bin:/infai/sieverss/local:/export/soft/lua_lmod/centos7/lmod/lmod/libexec:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:$PATH\nexport LD_LIBRARY_PATH=/scicore/soft/apps/binutils/2.32-GCCcore-8.3.0/lib:/scicore/soft/apps/cURL/7.66.0-GCCcore-8.3.0/lib:/scicore/soft/apps/bzip2/1.0.8-GCCcore-8.3.0/lib:/scicore/soft/apps/zlib/1.2.11-GCCcore-8.3.0/lib:/scicore/soft/apps/ncurses/6.1-GCCcore-8.3.0/lib:/scicore/soft/apps/GCCcore/8.3.0/lib64:/scicore/soft/apps/GCCcore/8.3.0/lib')
+if not project.REMOTE:
+    ENVIRONMENT = LocalEnvironment(processes=2)
 
 exp = project.get_evaluation_experiment(
     planners=PLANNERS,
     benchmarks_dir=BENCHMARKS_DIR,
     domains=DOMAINS,
     attributes=ATTRIBUTES,
-    time_limit=1800)
+    environment=ENVIRONMENT,
+    time_limit=1800, # 30 min
+    memory_limit=3584, # 3.5 GiB
+    )
 
 exp.run_steps()
